823e6ceef078bd517bd2b0fe7028bb77d914ea9d
Юлия Яцынина <julisa.milina@gmail.com>
2023-09-06 18:02:57 +0700

    вывод первой страницы лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app=Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Яцынина Юлия Сергеевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>Web-сервер на flask</h1>
+
+        <footer>
+            &copy; Юлия Яцынина, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
