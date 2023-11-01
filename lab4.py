30e0415fac8ce6b7d8821189e6126f2c9c58d3c3
Юлия Яцынина <julisa.milina@gmail.ru>
2023-11-01 14:49:36 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -2,7 +2,33 @@ from flask import Blueprint, render_template, request
 lab4=Blueprint('lab4', __name__)
 
 
-@lab4.route("/lab4/")
+@lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
 
+
+@lab4.route('/lab4/sucess')
+def uspeh():
+    return render_template('sucess.html')
+
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    if username == '':
+        error = 'Не введен логин'
+        return render_template('login.html', error=error, username=username)
+    password = request.form.get('password')
+    if password == '':
+        error = 'Не введен пароль'
+        return render_template('login.html', error=error, password=password)
+    
+    if username == 'alex' and password == '123':
+        return render_template('sucess.html', username=username, password=password)
+    else:
+        error = 'Неверный логин и/или пароль'
+        return render_template('login.html', error=error, username=username, password=password)
+    
