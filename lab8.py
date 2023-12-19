377a43c0a3c88602d9cbf40fc857639a9f921ffb
Юлия Яцынина <julisa.milina@gmail.com>
2023-12-19 19:43:02 +0700

    Редактирование выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -38,3 +38,13 @@ def del_course(course_num):
     return '', 204
 
 
+#изменение существующего курса
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_course(course_num):
+    course = request.get_json()
+    if course_num < 0 or course_num >= len(courses):
+        abort(404) 
+    courses[course_num] = course
+    return courses[course_num]
+
+
